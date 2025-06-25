6737289d46adada1ff3586caf24cda42a4c2d6e6
FilmyKannadiga <naveengowdathegreat@gmail.com>
2025-06-25 13:16:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://envs.sh/dIh.jpg https://envs.sh/dI2.jpg')).split()
+PICS = (environ.get('PICS', 'https://ibb.co/L3DDzVk')).split()
 
 
 # Admins & Users
