24855ff6312d25d808880ce65e5add1ca30329d8
FilmyKannadiga <naveengowdathegreat@gmail.com>
2025-06-25 11:27:55 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -111,7 +111,7 @@ Cᴏᴍᴍᴀɴᴅs Aɴᴅ Usᴀɢᴇ:
 <b>ɴᴏᴛᴇ:</b>
  <b>✯ Maintained by : <a href={}>Owner</a></b>
   
- <b>✯ Join here : <a href={}>Update Channel</a></b> 
+ <b>✯ Join here : <a href={}>https://t.me/+zfC7qxGkAAI1ZDM9</a></b> 
   
  ./id - <code>ɢᴇᴛ ɪᴅ ᴏꜰ ᴀ ꜱᴘᴇᴄɪꜰɪᴇᴅ ᴜꜱᴇʀ.</ 
  code> 
