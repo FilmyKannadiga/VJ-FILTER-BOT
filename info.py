5028141faeb6ae8be6ef863a8972bd9116dbead4
FilmyKannadiga <naveengowdathegreat@gmail.com>
2025-06-25 11:02:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -125,7 +125,7 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False)) # Set True Or False
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'Linkcents.com')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'f98edb38eb357ffc4b38c8a1bd5ae0cb51efb7e6')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/+oysDrXB04xgyZDZl') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
