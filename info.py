de1bb70775f127c2adf5a697b3adb15bfaf1e247
FilmyKannadiga <naveengowdathegreat@gmail.com>
2025-06-26 14:51:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,9 +88,9 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+zfC7qxGkAAI1ZDM9')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+LEBoz8OBWTc4ZDBl')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Kannada_Cinema_World')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '+LEBoz8OBWTc4ZDBl') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '+zfC7qxGkAAI1ZDM9') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/KCW_Admin_Bot')
 
 # True Or False
